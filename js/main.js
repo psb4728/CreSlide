b19d6ef0948b9cfc0c6c49ac8aebfc8f2712af5e
psb4728 <63276761+psb4728@users.noreply.github.com>
2023-04-26 16:16:04 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -49,6 +49,16 @@ $(function () {
     $(this).addClass('active');
     $('#' + gender_id).addClass('show');
   });
+
+  $('ul.gender_tab5 li').click(function () {
+    let gender_id = $(this).attr('data-gender');
+
+    $('ul.gender_tab5 li').removeClass('active');
+    $('.swiper.swiper5').removeClass('show');
+
+    $(this).addClass('active');
+    $('#' + gender_id).addClass('show');
+  });
 });
 
 // swiper
@@ -98,6 +108,21 @@ var swiper3 = new Swiper(".mySwiper3", {
 });
 
 var swiper4 = new Swiper(".mySwiper4", {
+  slidesPerView: 1,
+  loop: true,
+  effect: "fade",
+  speed: 500,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+
+var swiper4 = new Swiper(".mySwiper5", {
   slidesPerView: 1,
   loop: true,
   effect: "fade",
